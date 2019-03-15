1ac2936599a8b776b647e4c26c5ffb7f72b4050d
vipinsoni38@gmail.com <vipinsoni38@gmail.com>
2019-03-15 17:54:58 +0530

    bottom nav implemented

--- app/src/main/java/com/goldenboat/waymart/DataTypes/ProductDetails.java
+++ app/src/main/java/com/goldenboat/waymart/DataTypes/ProductDetails.java
@@ -1,10 +1,25 @@
 package com.goldenboat.waymart.DataTypes;
 
-import java.util.ArrayList;
 
 public class ProductDetails {
-    String id,pic_url,product_desc;
-    int price,rating;
-    ArrayList<String> cus_rating_ids;
+    String id="",pic_url="",product_title="";
+    int price=0;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPic_url() {
+        return pic_url;
+    }
+
+    public String getProduct_desc() {
+        return product_title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
 
 }
---
+++ app/src/main/java/com/goldenboat/waymart/Fragments/CartRecyclerAdapter.java
@@ -0,0 +1,61 @@
+package com.goldenboat.waymart.Fragments;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.goldenboat.waymart.DataTypes.ProductDetails;
+import com.goldenboat.waymart.R;
+import java.util.ArrayList;
+
+public class CartRecyclerAdapter extends RecyclerView.Adapter<CartRecyclerAdapter.CartViewHolder> {
+
+    ArrayList<ProductDetails> data;
+    Context mctx;
+
+    public CartRecyclerAdapter(Context mctx) {
+        this.mctx = mctx;
+    }
+
+    @NonNull @Override
+    public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_cart, parent, false);
+
+        return new CartViewHolder(v);
+    }
+
+    @Override public int getItemCount() {
+        return data.size();
+    }
+
+    public void givedata(ArrayList<ProductDetails> data) {
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+    @Override public void onBindViewHolder(@NonNull CartViewHolder holder, int position) {
+
+
+    }
+
+    public class CartViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView cardImage;
+        TextView title,price,count;
+
+        public CartViewHolder(View itemView) {
+            super(itemView);
+            cardImage = itemView.findViewById(R.id.item_image);
+            title = itemView.findViewById(R.id.object_title);
+            price = itemView.findViewById(R.id.price);
+            count = itemView.findViewById(R.id.quantity);
+
+        }
+
+    }
+
+}
