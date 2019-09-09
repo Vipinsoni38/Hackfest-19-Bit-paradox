c8c1f155462adfa07aec33c38f5422999270bb4d
ashwanikr7499 <34263444+ashwanikr7499@users.noreply.github.com>
2019-09-09 22:44:28 +0530

    Update nodemcu_code.ino

--- Hardware Code/nodemcu_code.ino
+++ Hardware Code/nodemcu_code.ino
@@ -1,62 +1,84 @@
-//nodemcu-code
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
 
-#include<SoftwareSerial.h>
-SoftwareSerial arduino(12,14);  //rx, tx // d6, d5
+//
+#define FIREBASE_HOST "example.firebaseio.com"
+#define FIREBASE_AUTH "token_or_secret"
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+SoftwareSerial arduino(D6, D5);
 char itemID[12];
-int count=0;
+int count = 0;
+char thisCartID[] = "CART0001";
+String items = "[";
+
+//SoftwareSerial arduino();
+
+
+/*
+ * this code will be uploaded in microcontroller of each WayCart.
+ * So this code must contain a unique-ID for its respective WayCart.
+ */
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  
+  //serial comm with arduino
   arduino.begin(9600);
+
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+
+  //firebase begin
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  //JSON data-frame creation
+  const size_t capacity = JSON_ARRAY_SIZE(20) + JSON_OBJECT_SIZE(1);
+  DynamicJsonBuffer itemsInCart(capacity);
+  JsonObject& root = itemsInCart.createObject();
+  JsonArray& items = root.createNestedArray("items");  
 }
 
-void loop() {
-/*  // put your main code here, to run repeatedly:
-  Serial.println(count);
-    if(arduino.available() && char(arduino.read())=='0')
-    
-    if(arduino.available() && char(arduino.read())=='8')
-    if(arduino.available()&& char(arduino.read())=='0')
-    if(arduino.available()&& char(arduino.read())=='0')
+void loop() 
+{
+  
+  if arduino.available()
+  {
+    //store ID string in a variable
+    count = 0;
+    while(arduino.available() && count<12)
     {
-      Serial.println(count);
-        while(arduino.available() && count<8)
-      {
-        itemID[count] = char(arduino.read());
-        count++;
-      }
-      Serial.println(String(itemID));
-      count=0;
-
+      itemID[count] = arduino.read();
+      count++;
+      delay(5);  
     }
-    else {itemID[0]=0; count++;}
+    //items = items + itemID;         //items.add(itemID);
     
-    delay(500); 
-    count=0;
-    
-    if(arduino.available() && arduino.read()==int("_"))
+    //Upload to Firebase
+    String s="";
+    for(int i=0;i<itemID.length();i++)
     {
-      while(arduino.available()=-=0p9ol8ki c && count<12)
-      {
-        itemID[count] = arduino.read();
-        count++;
-      }
-      //itemID = arduino.readString();
-      Serial.println(itemID);
-    }*/
-  //Serial.println(count);
-  //Serial.println("..................");
-  if(arduino.available())
-    {
-        while(arduino.available() && count<13)
-      {
-        itemID[count] = char(arduino.read());
-        count++;
-      }
-      Serial.println(itemID);
-      //sendToFirebase(purify(itemID));
-      count=0;
+      s+=itemID[i];
     }
-    delay(500); 
-    count=0;
+    //int ind=items.indexOf(',');
+    if( items.length()==1)
+    items+=s;
+    else
+    items=items+","+s;
+    
+    
+    Firebase.pushString("/carts/"+thisCartID, items+"]");
+  } 
 }
+
+//char json[] = "[1,2,3]";
+//JsonArray& array2 = jsonBuffer.parseArray(json);
